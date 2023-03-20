2253c52b337917de6297b76d077d19997865ea5c
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-03-21 00:26:44 +0200

    add - deletedArray.splice(1, 3);

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let newArray = elements.split(",").map(parseFloat).sort((a, b) => a - b);
 alert(`Ваш введенный массив: ${newArray}`);
 
 let [_, ...deletedArray] = newArray;
-deletedArray.splice(1, 2);
+deletedArray.splice(1, 3);
 
 alert(`Элементы с 2 по 4 удалены: ${deletedArray}`);
 console.log(deletedArray);
